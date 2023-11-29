b765b593cfdd3c8e37c4e7d5ea4a19ba8419c08c
viniciusdesouzalima.vsl <viniciusdesouzalima.vsl@gmail.com>
2023-11-29 16:18:42 -0300

    inicial

---
+++ src/main/java/com/mycompany/futureenergy/Empresa.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.futureenergy;
+
+/**
+ *
+ * @author Aluno
+ */
+public class Empresa {
+    
+    String nome;
+    String cnpj;
+    String usuario;
+    
+}
